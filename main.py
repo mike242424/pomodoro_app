6576ffb4abd84c90ad907f614111c9ffbb9d4a47
Michael Byers <mikebyers24@gmail.com>
2024-08-02 10:45:34 -0400

    added title label.

--- main.py
+++ main.py
@@ -19,12 +19,15 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro App")
-window.config(padx=100, pady=50, bg=YELLOW)
+window.config(padx=70, pady=50, bg=YELLOW)
+
+label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35))
+label.pack()
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 bg_img = PhotoImage(file="./tomato.png")
 canvas.create_image(100, 112, image=bg_img)
-canvas.create_text(103, 130, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
+canvas.create_text(100, 130, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
 canvas.pack()
 
 window.mainloop()
